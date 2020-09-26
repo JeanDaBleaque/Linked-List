9db4d051ee9bfbc4ddba068dae0b069c58ee20ea
Necati <mc.shiroistar@gmail.com>
2020-09-26 21:48:49 +0300

    Added remove from index and get command.

--- Linked_List/LinkedList.h
+++ Linked_List/LinkedList.h
@@ -13,6 +13,7 @@ public:
         Type value;
         Node *next = nullptr;
         Node *previous = nullptr;
+        int index;
     };
     int objectSize = 0;
     Node *head = nullptr;
@@ -27,26 +28,32 @@ public:
         } else {
             return false;
         }
-    }
+    };
     void add(Type objValue) {
         if (isEmpty()) {
             Node *nextNode = new Node(objValue);
             head = nextNode;
             tail = head;
+            nextNode->index = objectSize;
             objectSize++;
         } else {
             Node *nextNode = new Node(objValue);
             tail->next = nextNode;
             nextNode->previous = tail;
             tail = tail->next;
+            nextNode->index = objectSize;
             objectSize++;
         }
+    };
+    void pushToIndex(Type objValue, int index) {
+        //WIP
     };
     void remove(Node *ptr) {
         if (!isEmpty()) {
             Node *pointer = head;
             while (pointer != NULL) {
                 if (pointer == ptr) {
+                    decreaseTillEnd(pointer->next);
                     Node *prev = ptr->previous;
                     Node *next = ptr->next;
                     if (prev != NULL) {
@@ -68,6 +75,95 @@ public:
             }
         }
     };
+    void remove(int index) {
+        try {
+            if (index>=objectSize) {
+                throw 0;
+            } else if (index<0) {
+                throw 1;
+            }
+            int fromStart = index - 0;
+            int fromEnd = objectSize - index;
+            if (fromStart<=fromEnd) {
+                Node* selectedNode = head;
+                while (selectedNode->index != index) {
+                    selectedNode = selectedNode->next;
+                }
+                removeFromIndex(selectedNode);
+            } else {
+                Node* selectedNode = tail;
+                while (selectedNode->index != index) {
+                    selectedNode = selectedNode->previous;
+                }
+                removeFromIndex(selectedNode);
+            }
+        } catch (int error) {
+            switch (error) {
+            case 0:
+                std::cout << "The object which selected by index is nullptr!" << std::endl;
+                break;
+            case 1:
+                std::cout << "Selected index can't be negative!" << std::endl;
+                break;
+            }
+        }
+    };
+    void removeFromIndex(Node *selectedNode) {
+        decreaseTillEnd(selectedNode->next);
+        Node *prev = selectedNode->previous;
+        Node *next = selectedNode->next;
+        if (prev != NULL) {
+            prev->next = next;
+        }
+        if (next != NULL) {
+            next->previous = prev;
+        }
+        if (selectedNode == head) {
+            head = next;
+        } else if (selectedNode == tail) {
+            tail = prev;
+        }
+        delete(selectedNode);
+    };
+    void decreaseTillEnd(Node* startNode) {
+        if (startNode != NULL) {
+            startNode->index--;
+            decreaseTillEnd(startNode->next);
+        }
+    };
+    Node* get(int index) {
+        try {
+            if (index>=objectSize) {
+                throw 0;
+            } else if (index<0) {
+                throw 1;
+            }
+            int fromStart = index - 0;
+            int fromEnd = objectSize - index;
+            if (fromStart<=fromEnd) {
+                Node* selectedNode = head;
+                while (selectedNode->index != index) {
+                    selectedNode = selectedNode->next;
+                }
+                return selectedNode;
+            } else {
+                Node* selectedNode = tail;
+                while (selectedNode->index != index) {
+                    selectedNode = selectedNode->previous;
+                }
+                return selectedNode;
+            }
+        } catch (int error) {
+            switch (error) {
+            case 0:
+                std::cout << "The object which selected by index is nullptr!" << std::endl;
+                break;
+            case 1:
+                std::cout << "Selected index can't be negative!" << std::endl;
+                break;
+            }
+        }
+    };
     Node* getHead() {
         return head;
     };
@@ -78,7 +174,7 @@ public:
         if (!isEmpty()) {
             Node *pointer = head;
             while (pointer != NULL) {
-                std::cout << pointer->value << std::endl;
+                std::cout << "Value: " << pointer->value << " Index: " << pointer->index << std::endl;
                 pointer = pointer->next;
             }
         }
--- Linked_List/main.cpp
+++ Linked_List/main.cpp
@@ -9,9 +9,9 @@ int main()
     newList.add(2);
     newList.add(1);
     newList.printAll();
-    List<int>::Node *ptr = newList.getHead();
-    //cout << ptr->value << endl;
-    newList.remove(ptr);
+    List<int>::Node *ptr = newList.get(1);
+    std::cout << "selected value: " << ptr->value << std::endl;
+    newList.remove(1);
     newList.printAll();
     return 0;
 }
